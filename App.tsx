da589280f9517fae2db9216f0c8d16def2ae6b58
vemarav <vemarav@outlook.in>
2021-11-28 19:09:32 +0530

    added: ACTION_VIEW and video support

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Linking, Alert} from 'react-native';
+import Video from 'react-native-video';
 
 const App = (): React.ReactChild => {
+  const [uri, setUri] = React.useState('No Uri');
+
+  React.useEffect(() => {
+    const subscription = Linking.addEventListener(
+      'url',
+      ({url}: {url: string}) => setUri(url),
+    );
+
+    return () => subscription?.remove();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}></Text>
+      <Text style={styles.title}>{uri}</Text>
+      <Video
+        repeat
+        source={{uri}}
+        resizeMode={'contain'}
+        style={{width: '100%', height: 300, backgroundColor: '#000000'}}
+      />
     </View>
   );
 };
@@ -14,12 +32,12 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
     justifyContent: 'center',
     alignItems: 'center',
   },
   title: {
-    fontSize: 40,
-    color: 'black',
+    fontSize: 20,
+    color: '#707070',
+    margin: 30,
   },
 });
