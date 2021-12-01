1f13f50d6827094bc29fb4a6d0b0eae72d134063
vemarav <vemarav@outlook.in>
2021-12-01 22:13:40 +0530

    refactor pan & pinch gesture handler

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Linking, StatusBar, Dimensions} from 'react-native';
 import Video from 'react-native-video';
 import {
   GestureHandlerRootView,
-  PanGestureHandler,
-  PinchGestureHandler,
+  GestureDetector,
+  Gesture,
 } from 'react-native-gesture-handler';
 
 const WINDOW = Dimensions.get('screen');
 
 const App = (): React.ReactChild => {
+  const [isPaused, setPaused] = useState(true);
   const [uri, setUri] = React.useState('No Uri');
 
   React.useEffect(() => {
@@ -21,22 +22,51 @@ const App = (): React.ReactChild => {
     return () => subscription?.remove();
   }, []);
 
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => setPaused(!isPaused));
+
+  const pan = Gesture.Pan()
+    .maxPointers(1)
+    .onBegin(event => {
+      console.log('Pan#onBegin', event);
+    })
+    .onUpdate(event => {
+      console.log('Pan#onUpdate');
+    })
+    .onEnd(event => {
+      console.log('Pan#onEnd', event);
+    });
+
+  const pinch = Gesture.Pinch()
+    .onBegin(event => {
+      console.log('Pinch#onBegin', event);
+    })
+    .onUpdate(event => {
+      console.log('Pinch#onUpdate');
+    })
+    .onEnd(event => {
+      console.log('Pinch#onEnd', event);
+    });
+
+  const gestures = Gesture.Race(doubleTap, pan, pinch);
+
   return (
     <GestureHandlerRootView>
       <StatusBar hidden />
-      <PanGestureHandler minPointers={1} maxPointers={1}>
-        <PinchGestureHandler minPointers={2}>
-          <View style={styles.overlay}>
-            <Video
-              repeat
-              source={require('./assets/video.mp4')}
-              resizeMode={'contain'}
-              style={styles.video}
-              fullscreen
-            />
-          </View>
-        </PinchGestureHandler>
-      </PanGestureHandler>
+      <GestureDetector gesture={gestures}>
+        <View style={styles.overlay}>
+          <Video
+            repeat
+            fullscreen
+            paused={isPaused}
+            source={require('./assets/video.mp4')}
+            resizeMode={'contain'}
+            style={styles.video}
+            onLoad={data => console.log('onload', data)}
+          />
+        </View>
+      </GestureDetector>
     </GestureHandlerRootView>
   );
 };
