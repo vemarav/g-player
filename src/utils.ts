1e29ea8db507f7702d92a0201af9225a5f3c1ac0
vemarav <vemarav@outlook.in>
2021-12-03 01:03:49 +0530

    pause/play, slider forward, zoom

---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+export const getValue = (animated: any) => {
+  return animated.value ?? animated._value;
+};
+
+export const getTime = (seconds: number) => {
+  const _seconds = `${Math.floor(seconds) % 60}`.padStart(2, '0');
+  const _minutes = `${Math.floor(seconds / 60) % 60}`.padStart(2, '0');
+  const _hours = `${Math.floor(seconds / 3600) % 24}`.padStart(2, '0');
+  if (Number(_hours) > 0) {
+    return `${_hours}:${_minutes}:${_seconds}`;
+  } else {
+    return `${_minutes}:${_seconds}`;
+  }
+};
